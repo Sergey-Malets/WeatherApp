eade3fb03df28aff0a34ee4b728b2054febb9022
Malets_Sergey <maletssergey@gmail.com>
2022-08-18 13:46:24 +0300

    2.HW_new URl

--- weatherApp/weather/urls.py
+++ weatherApp/weather/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('',views.index),
+    path('',views.index, name='home'),
+    path('new_url/', views.new_URL, name='new_url')
 
 ]
--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'weather/index.html')
+
+def new_URL(request):
+    return render(request, "weather/new_url.html")
