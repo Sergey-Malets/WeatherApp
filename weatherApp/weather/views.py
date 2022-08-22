4e8e958aff34a5cb113034fe97401038f3887693
Malets_Sergey <maletssergey@gmail.com>
2022-08-22 13:43:18 +0300

    4.HW_вывести в терминал данные о погоде в Минске

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
+    appid = "22157536b45f94e241c43cc5f22072da"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = 'London'
+    res = requests.get(url.format(city))
+    res2 = requests.get(url.format('Minsk'))
+
+    print(res.text)
+    print(res2.text)
+
     return render(request, 'weather/index.html')
 
 def new_URL(request):
