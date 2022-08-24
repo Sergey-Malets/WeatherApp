9c9e6e2a69210888d69b2bdb641cc3c9fd0dfc42
Malets_Sergey <maletssergey@gmail.com>
2022-08-24 18:08:45 +0300

    5.HW_создать свой шаблон: index_2.html

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -7,16 +7,39 @@ def index(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
     city = 'London'
-    res = requests.get(url.format(city))
+    res = requests.get(url.format(city)).json()
     res2 = requests.get(url.format('Minsk'))
 
-    print(res.text)
-    print(res2.text)
+    city_info={
+        'city': city,
+        'temp':res['main']['temp'],
+    'icon': res['weather'][0]['icon']
+    }
+    context = {
+        'info': city_info
+    }
 
-    return render(request, 'weather/index.html')
+    return render(request, 'weather/index.html', context)
 
 def new_URL(request):
     return render(request, "weather/new_url.html")
 
 def index_2(request):
-    return render(request, 'weather/index_2.html')
+    appid = "22157536b45f94e241c43cc5f22072da"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+        'feels_like': res['main']['feels_like'],
+        'wind': res['wind']['speed'],
+    }
+    context = {
+        'info': city_info
+    }
+
+    return render(request, 'weather/index_2.html', context)
